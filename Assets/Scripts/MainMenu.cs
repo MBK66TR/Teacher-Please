ffe970b1dbea6297ae67a3bc1b06780d189c3220
Mustafa Bugra <mustafakaraturk66@gmail.com>
2025-02-26 09:55:20 +0300

    Save Point 10

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Menü Panelleri")]
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject creditsPanel;
+    
+    [Header("Menü Butonları")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button quitButton;
+    
+    [Header("Panel Butonları")]
+    [SerializeField] private Button backFromCreditsButton;
+    private void Start()
+    {
+        // Ana panel butonları
+        if (playButton != null)
+            playButton.onClick.AddListener(StartGame);
+        if (creditsButton != null)
+            creditsButton.onClick.AddListener(ShowCredits);
+        if (quitButton != null)
+            quitButton.onClick.AddListener(QuitGame);
+            
+        // Geri dönüş butonları
+        if (backFromCreditsButton != null)
+            backFromCreditsButton.onClick.AddListener(HideCredits);
+            
+
+        // Başlangıçta sadece ana paneli göster
+        ShowMainPanel();
+    }
+    
+    private void ShowMainPanel()
+    {
+        mainPanel?.SetActive(true);
+        creditsPanel?.SetActive(false);
+    }
+    
+    private void StartGame()
+    {
+        // Oyun sahnesine geç (Build Settings'de index 1 olarak ayarlanmalı)
+        SceneManager.LoadScene(1);
+    }
+    
+    private void ShowCredits()
+    {
+        mainPanel?.SetActive(false);
+        creditsPanel?.SetActive(true);
+    }
+    
+    private void HideCredits()
+    {
+        creditsPanel?.SetActive(false);
+        mainPanel?.SetActive(true);
+    }
+    
+        
+    private void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("GameVolume", volume);
+        PlayerPrefs.Save();
+    }
+    
+    private void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+    
+    private void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+} 
